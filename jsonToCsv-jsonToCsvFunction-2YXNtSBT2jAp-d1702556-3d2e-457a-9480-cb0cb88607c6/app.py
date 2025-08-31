456ce0d7bf0b34fcf64ea4840ac2c70589230bf6
Manojkumar Nagula <manojkumar.nagula@CHI-ML-MAkumar.local>
2025-08-31 20:27:32 +0530

    added logs in Json to CSV

--- jsonToCsv-jsonToCsvFunction-2YXNtSBT2jAp-d1702556-3d2e-457a-9480-cb0cb88607c6/app.py
+++ jsonToCsv-jsonToCsvFunction-2YXNtSBT2jAp-d1702556-3d2e-457a-9480-cb0cb88607c6/app.py
@@ -4,12 +4,16 @@ from io import StringIO
 import boto3
 from urllib.parse import unquote_plus
 from python_to_java_json_conversion import convert_format
+from constants.aws_config import aws_access_key_id,aws_secret_access_key
+from helpers.custom_logger import enable_custom_logging
+enable_custom_logging()
+
 
 
 
 s3 = boto3.client('s3', region_name='us-east-1',
-                  aws_access_key_id='',
-                  aws_secret_access_key='')
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key)
 
 def move_pdfs_for_digitization(bucket_name, file_name):
 
@@ -26,34 +30,41 @@ def lambda_handler(event, context):
     file_obj = event["Records"][0]
     bucket_name = str(file_obj["s3"]["bucket"]["name"])
     file_name = unquote_plus(str(file_obj["s3"]["object"]["key"]))
+    document_name = file_name.split('/')[-1].replace('.json','.pdf')
 
     print(file_name, bucket_name, 1)
 
+    print(f"reading the raw json file:- {document_name}")
     s3_clientobj = s3.get_object(Bucket=bucket_name, Key=file_name)
     s3_clientdata = s3_clientobj['Body'].read().decode('utf-8')
 
     jsn = json.loads(s3_clientdata)
 
     df1 = pd.DataFrame()
+    print(f"conversion of json to pandas df is started:- {document_name}")
     for x in jsn:
         df = pd.json_normalize(x['Blocks'])
         df1 = pd.concat([df1, df])
 
     csv_buf = StringIO()
     df1.to_csv(csv_buf, header=True, index=False)
-
     csv_buf.seek(0)
+    print(f"conversion of json to pandas df is compelted:- {document_name}")
+
     converted_file_name = file_name.replace(
         "raw-json/", "json-csv/").replace(".json", ".csv")
+    print(f"csv file is being saved:- {document_name}")
     resp = s3.put_object(Bucket=bucket_name, Body=csv_buf.getvalue(), Key=converted_file_name)
     print(f'output saved for json to csv conversion {converted_file_name}')
+    print(f"csv file is saved:- {document_name}")
+
 
     try:
-        print(f'Format conversion of python response to java response started: {file_name}')
+        print(f'format conversion of python response to java response started:- {document_name}')
         java_json_data = convert_format(jsn)
         stored_java_json = s3.put_object(Bucket=bucket_name, Body=json.dumps(java_json_data),
                                             Key=file_name.replace("raw-json/", "digitize-json/"))
-        print(f'Format conversion of python response to java response completed: {file_name}')
+        print(f'format conversion of python response to java response completed:- {document_name}')
 
         move_pdfs_for_digitization(bucket_name, file_name)
 
@@ -69,3 +80,23 @@ def lambda_handler(event, context):
             "message": "output saved..",
         }),
     }
+keys=[
+'devoted/zai_medical_records_pipeline/textract-response/raw-json/ed4b9177-3f4f-4aca-a203-3ce25b499bd1_AJX27GW4KC_IP1_20250703_152838.json'
+]
+for key in keys:
+    
+    event = {
+    "Records": [
+    {
+        "s3": {
+        "bucket": {
+            "name": "zai-revmax-qa"
+        },
+        "object": {                                                                           
+            "key": key
+        }
+        }
+    }
+    ]
+    }
+    lambda_handler(event, '')
