1e82a2d7973e9af5e5aee61977f902332d40b8b1
Manojkumar Nagula <manojkumar.nagula@CHI-ML-MAkumar.local>
2025-09-22 15:58:01 +0530

    corrected patter syntax

--- section_subsection/section_subsection/helpers/custom_logger.py
+++ section_subsection/section_subsection/helpers/custom_logger.py
@@ -32,7 +32,7 @@ def custom_print(*args, **kwargs):
     builtins._original_print(message, **kwargs)
 
     if ":-" in message:
-        pattern = r'(AJX[\w\-]{7}|H00[\w\-]{9}|V00[\w\-]{9}|200[\w\-]{4}'
+        pattern = r'(AJX[\w\-]{7}|H00[\w\-]{9}|V00[\w\-]{9}|200[\w\-]{4}))'
         ARL = re.findall(pattern, message, flags=re.IGNORECASE)
         ARL = ARL[0] if ARL else ''
         ARL = ARL.upper()
